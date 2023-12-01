2fecf06a15045363064086024b8e5a2fa1afc9b8
sagro <119251079+sagrodat@users.noreply.github.com>
2023-12-01 15:18:46 +0100

    Adding project, it was created locally

---
+++ Szachy2023V3/Tests.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "ChessGame.h"
+#include "myMouse.h"
+void testAmountofPossibleMoves1Deep(ChessGame chess);
+
+void testAmountofPossibleMoves2Deep(ChessGame chess);
+
+void doTestMoves(ChessGame chess);
+
+void windowRefresh(int ms);
+
+int testAmountOfPossibleMovesNDeep(ChessGame chess, int depth);
---
+++ Szachy2023V3/main.cpp
@@ -0,0 +1,98 @@
+#define WIN32_LEAN_AND_MEAN
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "ChessGame.h"
+#include "myMouse.h"
+#include "drawing.h"
+#include "tests.h"
+#include "sfmlObjects.h"
+#include "Printer.h"
+#include "SoundEffects.h"
+#include "AssetsPath.h"
+
+using namespace std;
+using namespace sf;
+
+//load assets from executable path using ModuleFileNameA
+int main()
+{
+	
+	
+	while (window.isOpen())
+	{
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+
+			case Event::KeyPressed :
+				if (Keyboard::isKeyPressed(Keyboard::R))
+				{
+					chess.revertMove(chess.moveLog.popLastMove());
+					chess.switchTurns();
+				}
+				if (Keyboard::isKeyPressed(Keyboard::K))
+				{
+					//printer.printPossibleMoves(chess.pieces.blackKnight1);
+				}
+				break;
+			case Event::Closed :
+				window.close();
+				break;
+
+			case Event::MouseButtonReleased :
+				if (chess.pieces.pressedPiece != nullptr)
+				{
+					MoveLog::move_t * attemptedMove = chess.moveLog.createMove( chess.pieces.pressedPiece, chess.pieces.pressedPiece->getStablePositionAsField() , mouse.getPosAsField() );
+					if (chess.isMoveLegal(attemptedMove))
+					{
+						chess.performMove(attemptedMove);
+						chess.playMoveSound(attemptedMove);
+						if (!chess.isEndOfGame())
+							chess.switchTurns();
+						
+						printer.print(chess.board);
+					}
+					else
+					{
+						chess.putPieceBackToStablePosition(chess.pieces.pressedPiece);
+					}
+					printer.print("\n\n");
+					chess.pieces.pressedPiece = nullptr;
+				}
+				else
+				{
+					printer.print("mouse released");
+				}
+				break;
+
+			case Event::MouseButtonPressed:	// for first iteration of mouse pressed down
+				chess.pieces.pressedPiece = mouse.isInBoundsOfPiece();
+				if (chess.pieces.pressedPiece != nullptr)
+				{
+					chess.onPiecePickup();
+				}
+				else
+				{
+					printer.print("mouse pressed");
+				}
+				break;
+			}
+		}
+
+		if (mouse.isButtonPressed(Mouse::Button::Left)) // for holding down
+		{
+			if (chess.pieces.pressedPiece != nullptr)
+			{
+				int newX = mouse.getPosition(window).x - mouse.mousePieceDelta.x;
+				int newY = mouse.getPosition(window).y - mouse.mousePieceDelta.y;
+				chess.movePiece(chess.pieces.pressedPiece, sf::Vector2f(newX, newY));
+			}
+		}
+
+		window.clear();
+		myDraw();
+		window.display();
+	}
+	return 0;
+}
